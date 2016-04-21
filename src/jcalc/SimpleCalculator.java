a0149e7a782227c09a2795784e51b66144d1d2b4
Ankit Pati <contact@ankitpati.in>
2016-04-22 05:18:37 +0530

    theme code cleanup

--- src/jcalc/SimpleCalculator.java
+++ src/jcalc/SimpleCalculator.java
@@ -21,7 +21,7 @@ package jcalc;
 
 import javax.swing.*;
 
-public class SimpleCalculator extends javax.swing.JFrame {
+public class SimpleCalculator extends JFrame {
     final static long serialVersionUID = 0l;
 
     public SimpleCalculator() {
@@ -476,12 +476,11 @@ public class SimpleCalculator extends javax.swing.JFrame {
     private void plafItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_plafItemStateChanged
         SwingUtilities.invokeLater(() -> {
             try {
-                for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
                     if (plaf.getSelectedItem().equals(info.getName())) {
                         UIManager.setLookAndFeel(info.getClassName());
                         break;
                     }
-                }
                 SwingUtilities.updateComponentTreeUI(this);
                 pack();
             }
@@ -567,28 +566,16 @@ public class SimpleCalculator extends javax.swing.JFrame {
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SimpleCalculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SimpleCalculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SimpleCalculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SimpleCalculator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-        //</editor-fold>
+        catch (Exception ex) {
+            System.err.println("Cannot set Nimbus theme.");
+        }
 
         /* Create and display the form */
         SwingUtilities.invokeLater(() -> new SimpleCalculator().setVisible(true));
